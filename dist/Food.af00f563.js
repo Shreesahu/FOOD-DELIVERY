87b68aa234fb7fed785a7559743de2378ba30327
Shreeya Sahu <shreeyasahu786@gmail.com>
2024-11-17 13:05:59 +0530

    deleting unwanted files

--- dist/Food.af00f563.js
+++
@@ -1,2 +0,0 @@
-var e=globalThis.parcelRequire91a2;(0,e.register)("gZbWU",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),l=()=>(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"WE ARE IN FOOD PAGE"})})});
-//# sourceMappingURL=Food.af00f563.js.map
